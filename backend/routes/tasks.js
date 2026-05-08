25a34f0e3ad32733b604d9dbd6def004de77d836
Shines Tech <148465479+Felicien407@users.noreply.github.com>
2026-05-08 12:57:42 +0200

    Day4: Implemented all backend request methods for tasks

--- backend/routes/tasks.js
+++ backend/routes/tasks.js
@@ -1,7 +1,10 @@
+import { error } from "console";
 import express from "express";
-import fs from "fs";
+import fs, { read, readSync } from "fs";
 import path from "path";
+import { title } from "process";
 import { fileURLToPath } from "url";
+import { v4 as uuidv4 } from "uuid";
 
 const router = express.Router();
 
@@ -9,19 +12,81 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const DATA_FILE = path.join(__dirname, "../data/tasks.json");
 
 const readTasks = () => {
-  fs.readFile(DATA_FILE, (error, data) => {
-    if (error) {
-      console.error(error);
-    }
-    console.log(data);
-    return data;
-  });
+  const tasks = fs.readFileSync(DATA_FILE, "utf-8");
+  return JSON.parse(tasks);
 };
 
-const tasks = readTasks();
+const writeTasks = (tasks) => {
+  fs.writeFileSync(DATA_FILE, JSON.stringify(tasks, null, 2));
+};
 
-router.get("/tasks", (req, res) => {
-  res.status(200).json({ data: tasks });
+const toogleCompleted = (tasks) => {};
+
+router.get("/", (req, res) => {
+  const tasks = readTasks();
+  res.status(200).json(tasks);
+});
+
+router.post("/", (req, res) => {
+  const { title } = req.body;
+
+  if (!title || !title.trim()) {
+    res.status(400, { message: "Title is required" });
+  }
+  const newTask = {
+    id: uuidv4(),
+    title: title.trim(),
+    completed: false,
+    createdAt: new Date().toISOString(),
+  };
+
+  const tasks = readTasks();
+  tasks.push(newTask);
+
+  writeTasks(tasks);
+  res.status(201).send({ message: "Task created successfully!" });
+});
+
+router.delete("/:id", (req, res) => {
+  const id = req.params.id;
+  const tasks = readTasks();
+
+  const index = tasks.findIndex((task) => task.id == id);
+  if (index === -1) {
+    return res.status(404).json({ message: "Task Not Found" });
+  }
+  tasks.splice(index, 1);
+  writeTasks(tasks);
+  res.status(204).send();
+});
+
+// ontoggle
+router.patch("/:id", (req, res) => {
+  const id = req.params.id;
+  const tasks = readTasks();
+  const task = tasks.find((task) => task.id == id);
+  if (!task) {
+    res.status(404).json({ message: "Task Not Found!" });
+  }
+  task.completed = !task.completed;
+  writeTasks(tasks);
+  res.status(200).json({ message: "Update successfully!", task: task });
+});
+
+router.put("/:id", (req, res) => {
+  const id = req.params.id;
+  const { title } = req.body;
+  const tasks = readTasks();
+  const updatedTask = {
+    id: uuidv4(),
+    title: title.trim(),
+    completed: false,
+    createdAt: new Date().toISOString(),
+  };
+  const task = tasks.find((task) => task.id == id);
+  tasks.splice(task, 1, updatedTask);
+  writeTasks(tasks);
+  res.status(200).json({ message: "Update successfully!", task: updatedTask });
 });
 
 export default router;
--- backend/server.js
+++ backend/server.js
@@ -7,10 +7,9 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.use(taskRouter);
+app.use("/api/tasks", taskRouter);
 
 const PORT = 7000;
 app.listen(PORT, () => {
   console.log(`Server is running on port http://localhost:${PORT}`);
 });
- 
